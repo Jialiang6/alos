e9eac5b216b5b2c0fba1a71fff347c9e53270122
Jialiang <liujliang6@mail2.sysu.edu.cn>
2023-09-09 20:29:26 +0800

    update create_thread

--- concurrency/create_thread.cpp
+++ concurrency/create_thread.cpp
@@ -18,27 +18,46 @@ void do_something_p(unique_ptr<X>) {
     cout << "func_using_p" << endl;
 }
 
+class Y {
+public:
+    void operator()(int x) {
+        cout << "func_" << endl;
+    }
+};
+
+class Z {
+public:
+    static void do_something_in(){
+        cout << "static_func_in_class" << endl;
+    }
+};
+
 int main() {
-    // thread t(do_something);
-    // thread t(&do_something);
+    // thread t(do_something); // function
+    // thread t(&do_something); // function pointer
 
     // X x;
-    // thread t(X::do_something_in, x);
+    // thread t(X::do_something_in, x); // non-static member function (type 1)
 
-    thread t(&X::do_something_in, X());
+    // thread t(&X::do_something_in, X()); // non-static member function (type 2)
 
-    // thread t(do_something_p, unique_ptr<X>());
+    // thread t(&Z::do_something_in); // static member function
 
-    // unique_ptr<X> p(new X);
-    // p->do_something_in();
-    // thread t(do_something_p, move(p));
+    thread t(Y(), 10); // class-op
 
-    // thread t([](){
+    // thread t([](){  // lambda function
     //     cout << "func_lambda" << endl;
     // });
 
-    // thread t0(do_something);
+
+    // thread t0(do_something); // thread can move like this
     // thread t(move(t0));
+
+    // unique_ptr show ...
+    // unique_ptr<X> p(new X);
+    // p->do_something_in();
+    // thread t(do_something_p, move(p));
+    // thread t(do_something_p, unique_ptr<X>());
 
     if (t.joinable()) t.join();
     // std::thread实例可移动，不可复制.
