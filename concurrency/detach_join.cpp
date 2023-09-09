a79ea99ac4f596c31d460cf412094ef90ffd92cd
Jialiang <liujliang6@mail2.sysu.edu.cn>
2023-09-09 20:45:46 +0800

    update detach_join

--- concurrency/detach_join.cpp
+++ concurrency/detach_join.cpp
@@ -15,12 +15,12 @@ void do_something(int len) {
 int main() {
     cout << "main()" << endl;
     thread t(do_something, 10);
-    t.detach();
+    t.detach(); // detach的作用是将t线程完全与main分离，即使main返回，t还能运行且不报错
     if (t.joinable()) // 2次detach直接退出
         t.detach();
 
-    // t.join();
-    // if (t.joinable())
+    // t.join();  // join的作用是阻塞main线程，直到t线程完成
+    // if (t.joinable()) // 2次join直接退出
     //     t.join();
 
     cout << "main() Done" << endl;
