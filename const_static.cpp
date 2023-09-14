4db1005bfaee6ed3298cdbba1d3a5d179b3234b0
Jialiang <liujliang6@mail2.sysu.edu.cn>
2023-09-14 17:35:45 +0800

    update const_static.cpp

--- const_static.cpp
+++ const_static.cpp
@@ -7,14 +7,30 @@
 #include <iostream>
 using namespace std;
 
-// 函数可近似看成全局变量，存储在代码段
+
+class A {
+    const int val;
+    constexpr A(): val(0) {}
+};
+
+/*
+    const/constexpr 修饰的名一定要初始化：
+        const可以修饰成员变量，但一定要在初始化列表初始化
+        constexpr可以修饰构造函数，但得在初始化列表初始化成员，构造函数内不能有成员的赋值
+*/
+
+
+// 函数和类可近似看成全局变量，存储在代码段
 
 int g = 0; // 全局变量，全局可见，外部可访问，生命周期为程序执行时，存储在数据段
 // int g; // 未初始化就存储在bss段
 const int cg = 0; // 全局只读变量，全局可见，外部可访问，生命周期为程序执行时，存储在数据段
+// const int cgg; // error: 必须初始化
 static int si = 0; // 静态全局变量，全局可见，外部不可访问，生命周期为程序执行时，存储在数据段
+// static int sii; // ok, bss
 static constexpr int cei = 0; // 显示声明更好，隐式也是static，全局可见，外部不可访问，生命周期程序，存储在数据段
-// constexpr int ceii; // error: 常量必须在声明时初始化，这是区分常量与只读变量的一大关键,另外常量在编译时就赋值
+// constexpr int ceii; // error: 常量必须在声明时初始化，常量与只读变量的区别是在编译时就赋值
+
 
 int main () {
     // 局部变量从存储上分，分为auto,static,register
@@ -27,5 +43,8 @@ int main () {
     const int cl = 0; // 局部只读变量，函数内可见，生命周期为函数，存储在栈
     static int sl = 0; // 静态局部变量，函数内可见，生命周期为函数，存储在数据段
     constexpr int cel = 0+1; // 可用常量表达式声明,用变量声明会报错,eg., x+1，必须初始化
+
+
     return 0;
 }
+
