3b4f89776ca78e1eb1d144a2fff4b0b25de31446
Jialiang <liujliang6@mail2.sysu.edu.cn>
2023-09-26 23:06:52 +0800

    update files

---
+++ concurrency/7_cv.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <thread>
+#include <condition_variable>
+using namespace std;
+/*
+    修改共享资源的部分容易理解；
+    wait部分较难理解，这里给出了wait部分的范本：
+     1. 【wait前先上锁mtx，wait之前会自动mtx.unlock()】
+        这是为了保证判断是否wait和wait这两个操作的原子性，不然可能漏掉消息，
+        判断要wait后，如果中间另一线程能修改共享资源，不用wait时，这时还wait就出错
+     2. 【推荐不光wait(mtx)，要加上pred】
+        提前满足pred->true时不wait提高效率，同时判断是否虚假唤醒
+        被唤醒后检查pred，如果pred->false就是虚假唤醒，此时继续等待
+        如果逻辑实现正确，虚假唤醒指的是硬件问题或者唤醒你后，
+        其他线程处理更快把pred改成false，此时你不应该被唤醒
+     3. 【从wait往下之前还隐性加mtx，获锁成功执行完代码块剩余代码后才释放mtx】
+        这样是为了确保notify_one代码块中剩余代码都执行完才轮到wait后代码的执行
+        或者为了保证notify_all实际上真的notify了all，让线程有序执行
+     4. 【notify_all唤醒多个线程一个时间只有一个线程获锁，必须一一有序执行剩余代码】
+
+     
+*/
+
+condition_variable cv;
+mutex mtx;
+bool ready = false;
+
+void print_id(int id) {
+    unique_lock<std::mutex> lck(mtx);
+    cv.wait(lck, []{ return ready; });
+    cout << "thread " << id << '\n';
+}
+
+void go() {
+    unique_lock<std::mutex> lck(mtx);
+    ready = true;
+    cv.notify_all();
+}
+
+int main() {
+    thread threads[10];
+    for (int i=0; i<10; ++i)
+        threads[i] = thread(print_id, i);
+
+    cout << "10 threads ready to race...\n";
+    go();
+
+    for (auto& th : threads) th.join();
+
+    return 0;
+}
---
+++ concurrency/8_waituntil.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <atomic>
+#include <condition_variable>
+#include <thread>
+#include <chrono>
+using namespace std::chrono_literals;
+// wait_for(mtx, rel_time, pred) == wiat_until(mtx, now+rel_time, pred)
+/*
+    wait_for往下走只有两种情况：
+     1）超时，则返回false
+     2）被唤醒，且检查pred->true，此时返回true
+    （如果被唤醒后发现pred->false，则继续等）
+*/
+
+std::condition_variable cv;
+std::mutex cv_m;
+int i;
+
+void waits(int idx)
+{
+    std::unique_lock<std::mutex> lk(cv_m);
+    if (cv.wait_for(lk, idx*100ms, []{return i == 1;})) {
+        std::cerr << "Thread " << idx << " finished waiting. i == " << i << '\n';
+    } else {
+        std::cerr << "Thread " << idx << " timed out. i == " << i << '\n';
+    }
+}
+
+void signals()
+{
+    std::this_thread::sleep_for(120ms);
+    std::cerr << "Notifying...\n";
+    cv.notify_all();
+    std::this_thread::sleep_for(100ms);
+    {
+        std::lock_guard<std::mutex> lk(cv_m);
+        i = 1;
+    }
+    std::cerr << "Notifying again...\n";
+    cv.notify_all();
+}
+
+int main()
+{
+    std::thread t1(waits, 1), t2(waits, 2), t3(waits, 3), t4(signals);
+    t1.join(); // t1 等待 100ms 后未被唤醒，自动超时；
+    t2.join(); // t2 在 120 ms 处被唤醒，但 condition 未满足，再此进入阻塞，200ms 时由于超时返回
+    t3.join(); // t3 在 120 ms 处被唤醒，但 condition 未满足，再此进入阻塞，220ms 时被 notify ，正常返回。
+    t4.join();
+}
+/*
+Thread 1 timed out. i == 0
+Notifying...
+Thread 2 timed out. i == 0
+Notifying again...
+Thread 3 finished waiting. i == 1
+*/
+
