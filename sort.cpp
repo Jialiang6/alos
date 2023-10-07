c644ea271e0bd2b1af0af9b2167c07bb548c542f
Jialiang <liujliang6@mail2.sysu.edu.cn>
2023-10-07 20:58:39 +0800

    update files

--- sort.cpp
+++ sort.cpp
@@ -24,14 +24,14 @@ void bubble_sort(vector<int>& nums) { // n2,n2,n,1,Y
     int n = nums.size();
     // 第几轮，每一轮确定一个最大值
     for (int i = 0; i < n-1; i++) {
-        bool ok = true;
+        bool NoChanage = true;
         for (int j = 0; j < n-i-1; j++) {
             if (nums[j] > nums[j+1]) {
                 swap(nums[j], nums[j+1]);
-                ok = false;
+                NoChanage = false;
             }
         }
-        if (ok) break;
+        if (NoChanage) break;
     }
 }
 
@@ -89,9 +89,65 @@ void heap_sort(vector<int>& nums) { // nlogn,nlogn,nlogn,1,N
     }
 }
 
-void merge_sort(vector<int>& nums) {}
+void merge_sort(vector<int>& nums) { // nlogn,nlogn,nlogn,n,Y
+    int n = nums.size();
+    vector<int> tmp(n); // 提前创建一个全局tmp数组，节省空间
+    // 分成[lft, mid]与[mid+1,rgt]两段，分别排序后再合并
+    function<void(int, int, int)> merge = [&](int lft, int mid, int rgt) {
+        int k = lft, i = lft, j = mid+1;
+        while (i <= mid && j <= rgt) {
+            if (nums[i] < nums[j]) {
+                tmp[k++] = nums[i++];
+            } else {
+                tmp[k++] = nums[j++];
+            }
+        }
+        while (i <= mid)
+            tmp[k++] = nums[i++];
+        while (j <= rgt)
+            tmp[k++] = nums[j++];
+        for (int i = lft; i <= rgt; i++)
+            nums[i] = tmp[i];
+    };
 
-void quick_sort(vector<int>& nums) {}
+    function<void(int, int)> MergeSort = [&](int lft, int rgt) {
+        if (lft < rgt) { // lft 有可能 == rgt
+            int mid = ((rgt - lft) >> 1) + lft;
+            MergeSort(lft, mid);
+            MergeSort(mid+1, rgt);
+            merge(lft, mid, rgt);
+        }
+    };
+
+    MergeSort(0, n-1);
+}
+
+void quick_sort(vector<int>& nums) { // nlogn,n2,nlogn,nlogn,N
+    int n = nums.size();
+    function<int(int, int)> partition = [&](int lft, int rgt) {
+        // randomPartition （随机一个数为基准数）
+        int i = rand() % (rgt - lft + 1) + lft; // 不随机的话i就固定是rgt
+        swap(nums[i], nums[rgt]); // 先把基准数放在最后rgt上
+        int x = nums[rgt], pivotIdx = lft; // x为基准数，pivotIdx是基准数最后的位置
+        for (int j = pivotIdx; j < rgt; j++) { // 小于基准数的放左边，大于的放右边
+            if (nums[j] < x) {
+                swap(nums[pivotIdx++], nums[j]); // 小于基准数的放到前面来
+            }
+        }
+        swap(nums[pivotIdx], nums[rgt]); // 将基准数放到该放的地方(左边数都小,右边数都大)
+        return pivotIdx;
+    };
+
+    function<void(int, int)> QuickSort = [&](int lft, int rgt) {
+        if (lft < rgt) {
+            int pivotIdx = partition(lft, rgt);
+            QuickSort(lft, pivotIdx - 1);
+            QuickSort(pivotIdx + 1, rgt);
+        }
+    };
+
+    QuickSort(0, n-1);
+}
 
 
 
@@ -105,7 +161,7 @@ int main() {
     }
     cout << endl;
 
-    shell_sort(nums);
+    merge_sort(nums);
     cout << "Sorted nums: " << endl;
     for (auto& num : nums) {
         cout << num << ' ';
